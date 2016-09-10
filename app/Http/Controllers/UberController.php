66aa94b02c4d79ee9a96a993c2eb89a6f3b940cc
JS0080 <js0080@yahoo.com>
2016-09-10 01:25:24 -0400

    revise save

--- app/Http/Controllers/UberController.php
+++ app/Http/Controllers/UberController.php
@@ -134,12 +134,11 @@ class UberController extends Controller
             return json_encode($result);
         }
 
-        $sql = "*, 3956 * 2 * ASIN(SQRT( POWER(SIN(($lat - abs(latitude)) * pi()/180 / 2),2) + COS($lat * pi()/180 ) 
- * COS(abs(latitude) * pi()/180) * POWER(SIN(($lng - longitude) * pi()/180 / 2),2) )) as distance from parkinglots having distance < 500000";
+        $sql = "select *, 3956 * 2 * ASIN(SQRT( POWER(SIN(($lat - abs(latitude)) * pi()/180 / 2),2) + COS($lat * pi()/180 ) 
+ * COS(abs(latitude) * pi()/180) * POWER(SIN(($lng - longitude) * pi()/180 / 2),2) )) as distance from parkinglots where 3956 * 2 * ASIN(SQRT( POWER(SIN(($lat - abs(latitude)) * pi()/180 / 2),2) + COS($lat * pi()/180 ) 
+ * COS(abs(latitude) * pi()/180) * POWER(SIN(($lng - longitude) * pi()/180 / 2),2) )) < 50000 ";
 
-        $parkinglot = DB::select("select *,  3956 * 2 * ASIN(SQRT( POWER(SIN((37 - abs(latitude)) * pi()/180 / 2),2) + COS(37 * pi()/180 ) 
- * COS(abs(latitude) * pi()/180) * POWER(SIN((37 - longitude) * pi()/180 / 2),2) )) as distance from parkinglots  where 3956 * 2 * ASIN(SQRT( POWER(SIN((37 - abs(latitude)) * pi()/180 / 2),2) + COS(37 * pi()/180 ) 
- * COS(abs(latitude) * pi()/180) * POWER(SIN((37 - longitude) * pi()/180 / 2),2) )) < 50000 ");
+        $parkinglot = DB::select($sql);
                             
         $result["status"] = "Ok";
         $result["places"] = $parkinglot;
