ac93da98ff0c95632532cc87455457b0aa961d73
JS0080 <js0080@yahoo.com>
2016-09-09 22:23:33 -0400

    test

--- app/Http/Controllers/UberController.php
+++ app/Http/Controllers/UberController.php
@@ -49,36 +49,61 @@ class UberController extends Controller
     }
 
     public function logintoken(Request $request){
-        $token = $request->get("access_token");
+        $token =  $request->get("access_token");
         $name  =  $request->get("name");
         $email = $request->get("email");
 
         $result["status"] = "error";
-        if (!empty($email) && !empty($token) && !empty($name)) {
-            $user = DB::table('uberusers')->where('email', $email)->first();
-
-            if (empty($user->name)) {
-                $newUser = new Uberuser;
-                $newUser->name = $name;
-                $newUser->email = $email;
-                $newUser->uber_credential = $token;
-                $newUser->save();
-            } else {
-                DB::table('uberusers')
-                    ->where('email', $email)
-                    ->update(['uber_credential' => $token]);
-            }
-
-            $result["status"] = "Ok";
+        if (empty($email) || empty($token) || empty($name)) {
+            return json_encode($result);
         }
+       
+        $user = DB::table('uberusers')->where('email', $email)->first();
+
+        if (empty($user->name)) {
+            $newUser = new Uberuser;
+            $newUser->name = $name;
+            $newUser->email = $email;
+            $newUser->uber_credential = $token;
+            $newUser->save();
+        } else {
+            DB::table('uberusers')
+                ->where('email', $email)
+                ->update(['uber_credential' => $token]);
+        }
+
+        $result["status"] = "Ok";
 
         return json_encode($result);
     }
 
      public function test(Request $request){
+        $token =  $request->get("access_token");
+        $name  =  $request->get("name");
+        $email = $request->get("email");
+
+        $result["status"] = "error";
+        if (empty($email) || empty($token) || empty($name)) {
+            return json_encode($result);
+        }
+       
+        $user = DB::table('uberusers')->where('email', $email)->first();
+
+        if (empty($user->name)) {
+            $newUser = new Uberuser;
+            $newUser->name = $name;
+            $newUser->email = $email;
+            $newUser->uber_credential = $token;
+            $newUser->save();
+        } else {
+            DB::table('uberusers')
+                ->where('email', $email)
+                ->update(['uber_credential' => $token]);
+        }
+
         $result["status"] = "Ok";
 
-          return json_encode($result);
+        return json_encode($result);
      }
 
     public function findnearby(Request $request){
