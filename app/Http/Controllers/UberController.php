d8ca9b80cdd640994f2923b3d27c050f353c1276
JS0080 <js0080@yahoo.com>
2016-09-30 08:14:05 -0400

    update savetoken

--- app/Http/Controllers/UberController.php
+++ app/Http/Controllers/UberController.php
@@ -37,9 +37,9 @@ class UberController extends Controller
         $result["status"] = "error";
         if (empty($email) || empty($token) || empty($name)) {
             $result["message"] = "Input is not proper";
-            $result["name"] => $name;
-            $result["email"] => $email
-            $result["token"] => $token;
+            $result["name"] = $name;
+            $result["email"] = $email
+            $result["token"] = $token;
             return json_encode($result);
         } else {
             $uberusers = DB::table('uberusers')
