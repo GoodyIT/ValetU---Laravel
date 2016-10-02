71a4158ed26d1896b270a3aa81c210828d8f1a5f
JS0080 <js0080@yahoo.com>
2016-10-02 09:24:20 -0400

    save token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -9,7 +9,6 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
 class VerifyCsrfToken extends BaseVerifier
 {
-	
     /**
      * The URIs that should be excluded from CSRF verification.
      *
@@ -18,4 +17,23 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except  = [
         'uber/v1/*',
     ];
+
+     protected $except_urls = [
+        'uber/v1/savereview',
+        'uber/v1/savetoken',
+        'uber/v1/saveparking',
+        'uber/v1/savecomment',
+    ];
+
+    public function handle($request, Closure $next)
+    {
+        $regex = '#' . implode('|', $this->except_urls) . '#';
+
+        if ($this->isReading($request) || $this->tokensMatch($request) || preg_match($regex, $request->path()))
+        {
+            return $this->addCookieToResponse($request, $next($request));
+        }
+
+        throw new TokenMismatchException;
+    }
 }
